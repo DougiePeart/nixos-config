bb674b8f8aa9d4793a9e621f48fe347b6c129b53
DougiePeart <d.peart@blackarrowgroup.io>
2023-02-10 19:23:26 +0000

    Started herbstluftwm config

--- modules/desktop/herbstluftwm/home.nix
+++ modules/desktop/herbstluftwm/home.nix
@@ -4,3 +4,42 @@
 # |  _  |  __/ |  | |_) \__ \ |_| | |_| |  _| |_ \ V  V /| | | | | |
 # |_| |_|\___|_|  |_.__/|___/\__|_|\__,_|_|  \__| \_/\_/ |_| |_| |_|
 #                                                                   
+
+{ config, lib, pkgs, host, ... };
+
+let
+  extra = ''
+    xsetroot -solid '#5A8E3A'
+
+    picom --config ~/.config/picom/picom.conf &
+    killall -q conky &
+    killall -q pasystray &
+    #conky &
+    bash /home/dougie/bin/polybar.sh &
+    emacs --daemon &
+    #feh --bg-scale /home/dougie/media/backgrounds/by_upload2_2560.jpg &
+    nitrogen --restore &
+    bash /home/dougie/.screenlayout/docked.sh
+    setxkbmap gb
+    pasystray &
+'';
+
+in
+{
+ xsession = {
+   enable = true;
+   numlock.enable = true;
+   windowManager = {
+     herbstluftwm = {
+       enable = true;
+       rules =;
+       keybinds =;
+       mousebinds =;
+       settings =;
+
+
+       }
+     }
+   }
+ }
+}
